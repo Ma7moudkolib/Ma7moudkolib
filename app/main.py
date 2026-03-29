de4d00d2ac46da3e077cdbd95030fc0669ac7d98
mahmoud kolib <mahmoud.kolib22@gmail.com>
2026-03-29 20:06:12 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -47,8 +47,8 @@ def main():
     print("Logs from your program will appear here!", file=sys.stderr)
 
     for tool_call in chat.choices[0].message.tool_calls:
-        if tool_call.type == "function" and tool_call.function_call.name == "Read":
-            file_path = tool_call.function_call.arguments["file_path"]
+        if tool_call.type == "function" and tool_call.function.name == "Read":
+            file_path = tool_call.function.arguments["file_path"]
             with open(file_path, "r") as f:
                 print(f.read())
             #tool_call.function_call.response = file_contents
