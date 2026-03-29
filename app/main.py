ee56e5262f3e8e0a7ad2aace0c186c102e6bba99
mahmoud kolib <mahmoud.kolib22@gmail.com>
2026-03-29 20:25:56 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -47,17 +47,15 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
 
-    for tool_call in chat.choices[0].message.tool_calls:
+    for tool_call in chat.choices[0].message.tool_calls or []:
         if tool_call.type == "function" and tool_call.function.name == "Read":
 
             args = json.loads(tool_call.function.arguments)
             file_path = args["file_path"]
             with open(file_path, "r") as f:
-                print(f.read())
-           # tool_call.function.response = file_contents
-           # print(tool_call.function.response)
+                print(f.read(),end="")
     # TODO: Uncomment the following line to pass the first stage
-    print(chat.choices[0].message.content)
+   # print(chat.choices[0].message.content)
 
 
 if __name__ == "__main__":
